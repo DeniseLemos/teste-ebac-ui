c7d0f86768993128e3445f213a0ec1e9a8704bef
Denise Lemos <exaud@uol.com.br>
2024-09-16 14:30:25 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,13 +2,35 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('deniseteste10@teste.com.br')
         cy.get('#password').type('teste@456')
         cy.get('.woocommerce-form > .button').click()
-
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deniseteste10 (não é deniseteste10? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deniseteste10 (não é deniseteste10? Sair')
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('maria@teste.com.br')
+        cy.get('#password').type('teste@456')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('exist')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('deniseteste10@teste.com.br')
+        cy.get('#password').type('teste@000')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail deniseteste10@teste.com.br está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+    });
 
 })
