ca4ff7338d69ead786cde15c1bc5632f1982385f
Denise Lemos <exaud@uol.com.br>
2024-10-30 15:48:17 -0300

    Acrescentando novos testes em login

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -15,15 +16,15 @@ describe('Funcionalidade: Login', () => {
         cy.get('#password').type('teste@456')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deniseteste10 (não é deniseteste10? Sair')
-    })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.get('#username').type('maria@teste.com.br')
+        cy.get('#username').type('deniseteste@teste.com.br')
         cy.get('#password').type('teste@456')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
         cy.get('.woocommerce-error').should('exist')
-    })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('deniseteste10@teste.com.br')
@@ -31,6 +32,23 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail deniseteste10@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deniseteste10 (não é deniseteste10? Sair')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario, { log: false })
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deniseteste10 (não é deniseteste10? Sair')
+        })
+
     });
 
 })
